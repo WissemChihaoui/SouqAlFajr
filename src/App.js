ddbb0ac37416a0985693fedbb87034fe825978f8
WissemChihaoui <chihahoui10@gmail.com>
2023-05-18 22:13:56 +0100

    Completed Edit and View Profile Screen + Responsive

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import PasswordReset from './en/pages/PasswordReset';
 import Home from './en/pages/Home';
 import PhoneVerifaction from './en/pages/PhoneVerifaction';
 import AboutUs from './en/pages/AboutUs';
+import Profile from './en/pages/Profile';
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Profile />
     </div>
   );
 }
---
+++ src/assets/lib/Countries.js
@@ -0,0 +1,10 @@
+const Countries = {
+        AF: "Afghanistan",
+        AL: "Albania",
+        DZ: "Algeria",
+        AS: "American Samoa",
+        AD: "Andorra",
+
+};
+
+export default Countries;
