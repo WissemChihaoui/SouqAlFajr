eb521b546e08f631c0e887d1a59a4f2d68982b69
WissemChihaoui <chihahoui10@gmail.com>
2023-05-19 01:59:23 +0100

    Completed Work

--- src/en/pages/Cart.js
+++ src/en/pages/Cart.js
@@ -3,7 +3,9 @@ import Navbar from '../comp/Navbar'
 import Footer from '../comp/Footer'
 import { Icon } from '@iconify/react';
 import onion from '../../assets/img/onion.jpg'
+import DeleteOrder from '../popups/DeleteOrder';
 const Cart = () => {
+    const [deleteOrder, setDeleteOrder] = useState(false)
     const [qte, setQte] = useState(0)
     const handleQte=(x)=>{
         let sum = x+qte;
@@ -12,20 +14,20 @@ const Cart = () => {
         }
       }
   return (
-    <div>
+    <div className='page'>
         <Navbar />
-        <div className='Cart'>
+        <div className='cart'>
             <div className='cartContainer'>
                 <div className='title'>
-                 <Icon icon="ic:outline-shopping-cart" />
-                 <h2>Cart</h2>
+                    <Icon icon="ic:outline-shopping-cart" />
+                    <h2>Cart</h2>
                 </div>
                 <div className='container'>
                     <ul>
                         <li>
                             <div className='details'>
                                 <div className='imageSection'>
-                                    <div className='deleteBtn'>
+                                    <div className='deleteBtn' onClick={()=>setDeleteOrder(true)}>
                                         <Icon icon="material-symbols:delete-outline" />
                                     </div>
                                     <img src={onion} />
@@ -60,8 +62,38 @@ const Cart = () => {
                     <Icon icon="la:shipping-fast" />
                     <h2>Shipping Options</h2>
                 </div>
+                <div className='shippingOptionsContainer'>
+                    <ul>
+                        <li>
+                            <label>Shipping Riyad</label>
+                            <span>1000 R.S</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className='invoiceDetails'>
+                    <div className='title'>
+                        <Icon icon="la:shipping-fast" />
+                        <h2>Invoice Details</h2>
+                    </div>
+                    <div className='invoiceDetailsContainer'>
+                        <ul>
+                            <li><label>Purchases</label><span className='importantText'>130 R.S</span></li>
+                            <li><label>Shipping</label><span className='importantText'>60 R.S</span></li>
+                            <li><label>Delivery</label><span className='importantText'>60 R.S</span></li>
+                            <li><label>Total</label><span className='importantText'>1000 R.S</span></li>
+                            <li>
+                                <Icon icon="mdi:warning-circle-outline" />
+                                <label>All prices including Taxes</label>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='paymentBtn'>
+                        <button>Payment</button>
+                    </div>
+                </div>
             </div>
         </div>
+        {deleteOrder && <DeleteOrder setDeleteOrder={setDeleteOrder}/>}
         <Footer />
     </div>
   )
---
+++ src/en/popups/DeleteOrder.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const DeleteOrder = ({setDeleteOrder}) => {
+  return (
+    <>
+        <div className='darkBG' onClick={() => setDeleteOrder(false)} />
+        <div className='centered modalDelete'>
+            <h2>Delete Order</h2>
+            <p>Are you sure to delete order <br/><b>Red Onion High Quality</b> ?</p>
+            <div className='buttons'>
+                <button className='cancel' onClick={() => setDeleteOrder(false)} >Cancel</button>
+                <button className='confirm'>Confirm</button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default DeleteOrder
